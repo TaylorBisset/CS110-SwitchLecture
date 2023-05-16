05ddaad19259b3d536b78587ee315f2cf253135a
Taylor Bisset <taylorbisset@gmail.com>
2023-05-16 10:13:07 -0700

    Code added. Used to practice adding and removing breaks.

--- CS110-SwitchLecture/CS110-SwitchLecture.cpp
+++ CS110-SwitchLecture/CS110-SwitchLecture.cpp
@@ -4,5 +4,24 @@ using namespace std;
 
 int main()
 {
-
+	int age = 0;
+	cout << "Please enter your age: ";
+	cin >> age;
+	switch (age) {
+	case 1:
+		cout << "You're one.";
+		break;
+	case 2:
+		cout << "You're two.";
+		break;
+	case 3:
+		cout << "You're three.";
+		break;
+	case 4:
+		cout << "You're four.";
+		break;
+	default:
+		cout << "You're older than four!";
+		break;
+	}
 }
